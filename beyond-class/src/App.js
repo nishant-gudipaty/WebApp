bc16fc8acb54131cec05f02a08c2df6009dc5a10
Visharad <visharadborsutkar777@gmail.com>
2021-11-07 19:25:17 +0530

    added login page

--- beyond-class/src/App.js
+++ beyond-class/src/App.js
@@ -1,22 +1,19 @@
 import './index.css';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import Home from './Home';
 import Test from './test';
+import LogIn from './LogIn';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return ( 
     <Router>
       <div className="App">
-        <Navbar/>
         <div className="content">
           <Switch>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route to='/test'>
-              <Test/>
-            </Route>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/test' component={Test}/>
+            <Route exact path='/login' component={LogIn}/>
           </Switch>
         </div>
       </div>
--- beyond-class/src/test.js
+++ beyond-class/src/test.js
@@ -1,16 +1,21 @@
 import './index.css';
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import 'react-bootstrap';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Test = () => {
+    
     return ( 
         <div className="test-content">
-            <Container>
+            <Container className = "bg-dark p-5">
                 <Row>
-                    <Col>column 1</Col>
-                    <Col>column 2</Col>
+                    <Col className='bg-primary'>
+                        <div className="h1">Column Title</div>
+                    </Col>
+                    <Col className = 'bg-info h1'>column 2</Col>
                 </Row>
             </Container>
         </div>
